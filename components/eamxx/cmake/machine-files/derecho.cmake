9dfe2c025ca320486c4b0878ea5f8acb33a1d626
Jian Sun <sunjian@ucar.edu>
2024-09-26 13:23:31 -0600

    add nvidiagpu compiler for Derecho to build Kokkos CUDA backend

--- components/eamxx/cmake/machine-files/derecho.cmake
+++ components/eamxx/cmake/machine-files/derecho.cmake
@@ -1,10 +1,18 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 common_setup()
 
-include (${EKAT_MACH_FILES_PATH}/kokkos/amd-zen3.cmake)
-include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
+if (USE_CUDA)
+  include (${EKAT_MACH_FILES_PATH}/kokkos/nvidia-a100.cmake)
+  include (${EKAT_MACH_FILES_PATH}/kokkos/cuda.cmake)
+else()
+  include (${EKAT_MACH_FILES_PATH}/kokkos/amd-zen3.cmake)
+  include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
+endif()
 include (${EKAT_MACH_FILES_PATH}/mpi/other.cmake)
 
+set(EKAT_MPI_EXTRA_ARGS "${EKAT_MPI_EXTRA_ARGS} --gpus-per-task=1" CACHE STRING "" FORCE)
+
+#option(Kokkos_ARCH_AMPERE80 "" ON)
 set(CMAKE_CXX_FLAGS "-DTHRUST_IGNORE_CUB_VERSION_CHECK" CACHE STRING "" FORCE)
 
 #message(STATUS "pm-cpu CMAKE_CXX_COMPILER_ID=${CMAKE_CXX_COMPILER_ID} CMAKE_Fortran_COMPILER_VERSION=${CMAKE_Fortran_COMPILER_VERSION}")
